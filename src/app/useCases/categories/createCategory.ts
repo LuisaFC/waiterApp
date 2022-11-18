8da72fcb35f4a379fc15688599eea1fc0e9d27ec
LuisaFC <luisabsb@gmail.com>
2022-11-18 18:13:20 -0300

    list and create categories

---
+++ src/app/useCases/categories/createCategory.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+
+import { Category } from '../../models/Category';
+
+export async function createCategory(req: Request, res: Response) {
+  try{
+    const {icon, name} = req.body;
+
+    const category = await Category.create({icon, name});
+
+    res.status(201).json(category);
+  }
+  catch(err){
+    console.log(err);
+    res.sendStatus(500);
+  }
+}
