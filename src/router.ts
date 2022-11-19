48a82a76cc077138f4fa6334b0f92f16f44d806f
LuisaFC <luisabsb@gmail.com>
2022-11-18 21:40:23 -0300

    create and list orders

--- src/router.ts
+++ src/router.ts
@@ -8,6 +8,8 @@ import { listCategories } from './app/useCases/categories/listCategories';
 import { createProduct } from './app/useCases/products/createProducts';
 import { listProducts } from './app/useCases/products/listProducts';
 import { listProductsByCategory } from './app/useCases/categories/listProductsByCategory';
+import { listOrders } from './app/useCases/orders/listOrders';
+import { createOrder } from './app/useCases/orders/createOrder';
 
 export const router = Router();
 
@@ -39,14 +41,10 @@ router.post('/products', upload.single('image'),  createProduct);
 router.get('/categories/:categoryId/products', listProductsByCategory);
 
 //List orders
-router.get('/orders', (req, res) => {
-  res.send('ok');
-});
+router.get('/orders', listOrders);
 
 //Create order
-router.post('/orders', (req, res) => {
-  res.send('ok');
-});
+router.post('/orders', createOrder,);
 
 //Change order status
 router.patch('/orders/orderId', (req, res) => {
